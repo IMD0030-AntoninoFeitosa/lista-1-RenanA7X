71984da83575692e279d8787b7f5a77e32356e30
RenanA7X <71436328+RenanA7X@users.noreply.github.com>
2022-04-12 21:25:26 -0300

    Add files via upload

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -2,8 +2,29 @@
 
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    // TODO: adicione o seu código aqui.
+    vector<unsigned int> fibo {};
+    int n1 = 1, n2 = 1;
+    int fib = 0;
 
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    //Adiciona para o fim do vetor
+    fibo.push_back(n1);
+    fibo.push_back(n2);
+  
+    while(1)
+    {
+      fib = n1 + n2;//Soma
+      if(fib < n)
+      {
+        fibo.push_back(fib);//Adiciona para o fim do vetor
+      }
+      else
+      {
+        break;
+      }
+      //Repassando valores
+      n1 = n2;
+      n2 = fib;
+    }
+
+    return fibo;
 }
