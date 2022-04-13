bd252167cfd3577db836609f0d498791c0fad500
RenanA7X <71436328+RenanA7X@users.noreply.github.com>
2022-04-12 21:30:49 -0300

    Update main.cpp

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -6,7 +6,20 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
+    int numeros[SIZE];//Vetor para a entrada
+    int negativos = 0;//Contador
+  
+    for (int i = 0; i < SIZE; ++i)//Laço do tamando SIZE
+    {
+      std::cin >> numeros[i];
+
+      if(numeros[i] < 0){//Se menor que 0 (negativo)
+         ++negativos;    //incrementa +1
+       }
+
+    }
+
+    std::cout << negativos;//Saída
 
     return 0;
 }
